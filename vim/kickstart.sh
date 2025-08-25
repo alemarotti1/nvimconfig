288dd538be95bd85abb23e1777145eb6a77756ce
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2025-08-25 17:07:31 -0300

    Altering code from the 'base' file so that the order of calling the 'Plug' script is correct

--- vim/kickstart.sh
+++ vim/kickstart.sh
@@ -4,3 +4,5 @@ printf "set runtimepath=${PWD}\n" >> .vimrc
 printf "set packpath=${PWD}\n" >> .vimrc
 printf "let g:_plug_path = \"${PWD}/plugged\"\n" >> .vimrc
 
+
+printf "\ncall plug#begin(g:_plug_path)\n Plug 'tpope/vim-sensible'\n Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }\n Plug 'junegunn/fzf.vim'\n Plug 'AmberLehmann/candyland.nvim'\n call plug#end()" >> .vimrc
