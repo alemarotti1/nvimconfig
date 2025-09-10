7ad64d01c5eb0f6510aebe61083a0d535c4d3451
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2025-09-10 00:03:56 -0300

    mudanças para corrigir os erros na configuração neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,117 @@
-C:/Users/pvp_m/AppData/Local/nvim/init.lua
+vim.cmd('source ~/AppData/Local/nvim/vimrc.vim')
+
+require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- Configurações básicas do vim 
+	use 'tpope/vim-sensible'
+
+
+	-- Configuração LSP
+	use({
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
+
+	-- FuzzyFinder - Plugin para encontrar arquivos d forma fácil
+	use {
+		'junegunn/fzf.vim',
+		requires = { 'junegunn/fzf', run = ':call fzf#install()' }
+	}
+
+	-- Instalação do Coq, utilizado para completar código
+	use 'ms-jpq/coq_nvim'
+	use 'ms-jpq/coq.artifacts'
+	use 'ms-jpq/coq.thirdparty'
+
+
+
+	-- Instalação do esquema de cores pastel
+	use 'AmberLehmann/candyland.nvim'
+end)
+
+
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+
+
+local lspconfig = require('lspconfig')
+lspconfig.ast_grep.setup({})
+lspconfig.ts_ls.setup({})
+lspconfig.rust_analyzer.setup({
+	on_attach = function(client, bufnr)
+		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+	end,
+	settings = {
+		["rust-analyzer"] = {
+			imports = {
+				granularity = {
+					group = "module",
+				},
+				prefix = "self",
+			},
+			cargo = {
+				buildScripts = {
+					enable = true,
+				},
+			},
+			procMacro = {
+				enable = true
+			},
+		}
+	}
+})
+
+local on_attach = function(client, bufnr)
+	local opts_buffer = { noremap = true, silent = true, buffer = bufnr }
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Mappings: LSP
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts_buffer)
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts_buffer)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts_buffer)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts_buffer)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts_buffer)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts_buffer)
+	vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts_buffer)
+	vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts_buffer)
+	vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts_buffer)
+	vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts_buffer)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts_buffer)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts_buffer)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts_buffer)
+	vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts_buffer)
+end
+
+local lsp_flags = {
+	debounce_text_changes = 150,
+}
+
+
+require('lspconfig')['ts_ls'].setup({
+	-- LSP-default-keymaps
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
+
+
+require('lspconfig')['lua_ls'].setup({
+	-- LSP-default-keymaps
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
+
+
+
+--Definição do color scheme pastel
+vim.cmd.colorscheme "candyland"
--- nvim/vimrc.vim
+++ nvim/vimrc.vim
@@ -1,1 +1,107 @@
-C:/Users/pvp_m/AppData/Local/nvim/vimrc.vim
+if v:progname =~? "evim"
+  finish
+endif
+
+
+if has("vms")
+  set nobackup		" do not keep a backup file, use versions instead
+else
+  set backup		" keep a backup file (restore to previous version)
+"  if has('persistent_undo')
+"    set undofile	" keep an undo file (undo changes after closing)
+"  endif
+endif
+
+
+if &t_Co > 2 || has("gui_running")
+  " Switch on highlighting the last used search pattern.
+  set hlsearch
+endif
+
+
+" Put these in an autocmd group, so that we can delete them easily.
+augroup vimrcEx
+  au!
+
+
+  " For all text files set 'textwidth' to 78 characters.
+  autocmd FileType text setlocal textwidth=78
+augroup END
+
+
+" Add optional packages.
+"
+" The matchit plugin makes the % command work better, but it is not backwards
+" compatible.
+" The ! means the package won't be loaded right away but when plugins are
+" loaded during initialization.
+if has('syntax') && has('eval')
+  packadd! matchit
+endif
+
+
+
+""""""""""""""""""""""""""""""""" PLUGINS """""""""""""""""""""""""""""""""""
+" call plug#begin()
+" 
+" " List your plugins here
+" Plug 'tpope/vim-sensible'
+" 
+" " Fuzzy finder - 
+" Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+" Plug 'junegunn/fzf.vim'
+" 
+" call plug#end()
+
+"comentado pois estamos usando o packer com nvim
+
+
+
+""""""""""""""""""""""""""""Configurações alê """"""""""""""""""""""""""""""
+set ts=2
+set sts=2
+set shiftwidth=2
+set noexpandtab
+
+
+syntax on
+set relativenumber "Adiciona numero de linhas relativas no lado esquerdo
+set cursorline "Adiciona marcação da linha atual
+set mouse=a "Adiciona a possibilidade de clicar onde você quer editar
+set nocompatible "Ativa o modo de não-compatibilidade - no geral não é necessário
+let mapleader = ' ' 
+
+vnoremap  <leader>y  "+y
+vnoremap  <leader>p  "+p
+
+
+set noundofile " Evita a criação de um arquivo de undo que persiste após fechar
+"Caso você queira utilizar o arquivo de undo, mas não poluir o diretório atual
+"Você pode usa os seguintes comandos:
+"set undodir=~/.vim/undo-dir
+"set undofile
+
+set nobackup " Impede a criação de um arquivo permanente de backup
+set writebackup " Permite a criação de um arquivo temporário de backup
+
+"faz com que a seta de "baixo" e "cima" vá para baixo/cima e centralize a tela
+nmap <Down> gjzz 
+nmap <Up> gkzz 
+
+" cria remapings para "q" ser equivalente a "aspas"
+nnoremap viq vi" 
+nnoremap vaq va"
+
+nnoremap <leader><C-e> :Explore<CR>
+nnoremap <leader>9y "9y
+nnoremap <leader>9p "9p
+
+"Criação de mapping que façam a inserção de abas de fechamento junto à tag de abertura
+inoremap " ""<Left>
+inoremap ( ()<Left>
+inoremap { {}<Left>
+inoremap [ []<Left>
+inoremap ` ``<Left>
+inoremap < < ><Left>
+inoremap ' ''<Left>
+
